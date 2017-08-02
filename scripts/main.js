34fb06fd1845201eee15f17364bf8856fb4aa41e
PWRMind <pwrmind@bk.ru>
2017-08-02 21:48:49 +0300

    add main css & js

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,29 @@
-setInterval(function(){
-	
-},1000);
+var tiles = document.querySelectorAll(".tile");
 
-function Event(name, date) {
-	var event = this;
-	event.date = date;
-	event.name = name;
-	event.getTimeLeft = function(){
-		return Date.now() - event.date; 
-	};
-	return this;
-}
-
-function Tile(event) {
-	var tile = this;
-	tile.event = new Event("Новый год", );
+function getDistance(countDownDate) {
+	var now = new Date().getTime();
+	var tmp = new Date((new Date()).getFullYear() + "." + countDownDate).getTime();
+	var countDownDate = (tmp > now) ?  tmp : new Date(((new Date()).getFullYear() + 1) + "." + countDownDate).getTime();
+    var distance = countDownDate - now;
 	return {
-		title: "",
-		date: ""
+		days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+    	hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    	minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+    	seconds: Math.floor((distance % (1000 * 60)) / 1000)
 	};
 }
+
+var x = setInterval(function() {
+    for (var index in tiles) {
+    	var tile = tiles[index];
+    	var countDownDate = tile && tile.dataset && tile.dataset.date;
+    	var name = tile && tile.dataset && tile.dataset.name;
+    	var distance = getDistance(countDownDate)
+    	tile.innerHTML = "<div class='name'>" + name + "</div>" + "<div class='days'>" + distance.days + " дней</div>" + "<div class='time'>" + distance.hours + ":" + distance.minutes + ":" + distance.seconds + "</div>";
+    };
+    
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("tiles").innerHTML = "EXPIRED";
+    }
+}, 1000);
