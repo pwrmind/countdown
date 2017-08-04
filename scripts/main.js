47753b041e8d09c4f643e9a7faa9ea49972e220a
PWRMind <pwrmind@bk.ru>
2017-08-04 14:31:29 +0300

    add same events

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,12 @@
 		var now = new Date().getTime();
 		var tmp = new Date((new Date()).getFullYear() + "." + countDownDate).getTime();
 		var countDownDate = (tmp > now) ?  tmp : new Date(((new Date()).getFullYear() + 1) + "." + countDownDate).getTime();
-	    var distance = countDownDate - now;
+	    var distance = Math.floor((countDownDate - now) / (1000 * 60 * 60 * 24));
+	    if(distance === 364) {
+	    	return { days: "Сегодня" }
+	    };
 		return {
-			days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+			days: distance + " дней",
 	    	// hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
 	    	// minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
 	    	// seconds: Math.floor((distance % (1000 * 60)) / 1000)
@@ -24,7 +27,7 @@
 	    	var countDownDate = tile && tile.dataset && tile.dataset.date;
 	    	var name = tile && tile.dataset && tile.dataset.name;
 	    	var distance = getDistance(countDownDate)
-	    	tile.innerHTML = "<div itemprop='name' class='name'>" + name + "</div>" + "<div class='days'>" + distance.days + " дней</div>";
+	    	tile.innerHTML = "<div itemprop='name' class='name'>" + name + "</div>" + "<div class='days'>" + distance.days + "</div>";
 	    	// + "<div class='time'>" + distance.hours + ":" + distance.minutes + ":" + distance.seconds + "</div>";
 	    };
 	    
